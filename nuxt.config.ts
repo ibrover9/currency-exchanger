5b6c7442d2212fc27cb427a593b84ebf580b125e
Artur Novikov <artur.20177102@mail.ru>
2024-06-30 13:33:00 +0300

    Adding the necessary tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,15 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+// export default defineNuxtConfig({
+//   devtools: { enabled: true }
+// })
+export default {
+  devtools: { enabled: true },
+  build: {
+    loaders: {
+      scss: {
+        implementation: require("sass"),
+      },
+    },
+  },
+  css: ["~/assets/main.scss"],
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,15 @@
 <template>
-  <div>111</div>
+  <div>
+    <input type="text" placeholder="Enter a number" />
+    <input type="text" placeholder="Select a currency" list="currency" />
+    <input type="text" />
+    <input type="text" placeholder="Select a currency" list="currency" />
+    <datalist id="currency">
+      <option value="Евро">Евро</option>
+      <option value="Доллар">Доллар</option>
+      <option value="Рубль">Рубль</option>
+    </datalist>
+  </div>
+
+  <!-- Third input field -->
 </template>
